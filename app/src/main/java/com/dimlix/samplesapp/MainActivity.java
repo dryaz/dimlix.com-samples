8e200b18a80e0c284070dfa57e0eb5071a4d2821
Dmitry <dmitry.ryazantsev@sonect.ch>
2019-10-17 11:31:29 +0300

    Add link to resources

--- app/src/main/java/com/dimlix/samplesapp/MainActivity.java
+++ app/src/main/java/com/dimlix/samplesapp/MainActivity.java
@@ -1,7 +1,10 @@
 package com.dimlix.samplesapp;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Pair;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -22,11 +25,45 @@ public class MainActivity extends AppCompatActivity {
         list.setLayoutManager(new LinearLayoutManager(this,
                 LinearLayoutManager.VERTICAL, false));
         list.setAdapter(new SamplesAdapter(getSamples()));
+
+        findViewById(R.id.imgTelegram).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openWeb("https://t.me/droDev");
+            }
+        });
+
+        findViewById(R.id.imgFacebook).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openWeb("https://www.facebook.com/droDev");
+            }
+        });
+
+        findViewById(R.id.imgVkontakte).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openWeb("https://vk.com/dr_dev");
+            }
+        });
+
+        findViewById(R.id.imgWeb).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openWeb("https://dimlix.com");
+            }
+        });
     }
 
     private List<Pair<String, Class>> getSamples() {
         List<Pair<String, Class>> samples = new ArrayList<>();
         samples.add(new Pair<String, Class>(getString(R.string.network_monitoring), NetworkMonitoringActivity.class));
         return samples;
+    }
+
+    private void openWeb(String url) {
+        Intent i = new Intent(Intent.ACTION_VIEW);
+        i.setData(Uri.parse(url));
+        startActivity(i);
     }
 }
